fb66663db567bd7a12eef1f0bc59c43e07a2352e
valentramos <ignacio1994.ir@gmail.com>
2018-08-28 00:37:43 -0400

    RESTServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,16 @@
 //====== PUERTO ======//
-process.env.PORT = process.env.PORT || 3001;
+process.env.PORT = process.env.PORT || 3000;
+
+//====== ENTORNO ======//
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//====== BASE DE DATOS ======//
+let urlDB;
+
+if (process.env.NODE_DEV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://valentramos:lizig66cg@ds263988.mlab.com:63988/cafe';
+}
+
+process.env.URLDB = urlDB;
