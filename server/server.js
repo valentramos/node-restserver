d15c66093ec78152f0e555f310ede8ebde6f0d7b
valentramos <ignacio1994.ir@gmail.com>
2018-08-28 23:14:51 -0400

    conexion a produccion

--- server/server.js
+++ server/server.js
@@ -17,8 +17,8 @@ app.use(bodyParser.json())
 app.use(require('./routes/index'));
 
 
-mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
-    // mongoose.connect(process.env.URLDB, (err, res) => {
+// mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
+mongoose.connect(process.env.URLDB, (err, res) => {
     if (err) throw err;
     console.log('Base de datos ONLINE!');
 });
