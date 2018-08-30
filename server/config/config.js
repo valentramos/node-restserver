2a35ad9f042e09e8e695a64382367c985ec31e84
valentramos <ignacio1994.ir@gmail.com>
2018-08-29 21:39:04 -0400

    google Sign In

--- server/config/config.js
+++ server/config/config.js
@@ -18,5 +18,7 @@ if (process.env.NODE_ENV === 'dev') {
 } else {
     urlDB = process.env.MONGO_URI;
 }
-
 process.env.URLDB = urlDB;
+
+//====== GOOGLE CLIENT ID ======//
+process.env.CLIENT_ID = process.env.CLIENT_ID || '589979426269-5s1m6neff9v94kvbf3m3i76230a3dp54.apps.googleusercontent.com';
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('./config/config');
 
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
@@ -13,9 +14,11 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+//Habilitar la carpeta public
+app.use(express.static(path.resolve(__dirname, '../public')));
+
 //Configuración global de rutas
 app.use(require('./routes/index'));
-
 
 // mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
 mongoose.connect(process.env.URLDB, (err, res) => {
